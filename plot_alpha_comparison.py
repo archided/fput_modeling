d7d4b34af2a0a1f158aabaac4b01492b6a109227
archided <ant.yuferova@gmail.com>
2026-06-23 07:02:00 +0300

    add plots

---
+++ plot_alpha_comparison.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from fput_simulation import simulate_fput
+
+N = 16
+dt = 0.01
+T_max = 4000
+alphas = [0, 0.05, 0.25, 0.5, 1.0]
+
+plt.figure(figsize=(10, 6))
+for a in alphas:
+    times, E_modes, _ = simulate_fput(N, a, dt, T_max)
+    plt.plot(times, E_modes[0], label=f'α = {a}')
+plt.xlabel('Время')
+plt.ylabel('Энергия первой моды')
+plt.title('Сравнение возврата при различных α')
+plt.legend()
+plt.grid(True)
+plt.savefig('alpha_comparison.png', dpi=300, bbox_inches='tight')
+plt.show()
---
+++ plot_reccurence.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from fput_simulation import simulate_fput
+
+N = 16
+alpha = 0.25
+dt = 0.01
+T_max = 2000
+
+times, E_modes, _ = simulate_fput(N, alpha, dt, T_max)
+
+plt.figure(figsize=(10, 6))
+for k in range(min(5, N)):
+    plt.plot(times, E_modes[k], label=f'{k+1} мода')
+plt.xlabel('Время')
+plt.ylabel('Энергия')
+plt.title(f'Возврат энергии в цепочке FPUT (N={N}, α={alpha})')
+plt.legend()
+plt.grid(True)
+plt.savefig('alpha025.png', dpi=300, bbox_inches='tight')
+plt.show()
